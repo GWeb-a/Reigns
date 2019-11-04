3818db28890f66250f1b28cdcbfe83c648ee0b09
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2019-11-04 08:05:53 +0100

    add get end and start to add a get with parameter

--- routes/getters.js
+++ routes/getters.js
@@ -11,14 +11,37 @@ class   Getters {
     	res.send('Hello World!\nthis is a get request' + 'And it is :' + theDate + '\nThere is no route for /')
     });
 
+    this.app.get('/cards:name?', (req, res) => {
+        console.log("request GET /cards with a parameter");
+        console.log("req body")
+        console.log(req.query)
+        const cards_res = this.db_mongo.collection('cards')
+        cards_res.find().toArray((err, items) => {
+          // console.log(items)
+          res.send(items)
+      })
+      // res.send(all_cards);
+    });
+
     this.app.get('/cards', (req, res) => {
         console.log("request GET /cards");
+        console.log(req)
         const cards_res = this.db_mongo.collection('cards')
         cards_res.find().toArray((err, items) => {
       		// console.log(items)
       		res.send(items)
     	})
     	// res.send(all_cards);
+    });
+
+    this.app.get('/ends', (req, res) => {
+        console.log("request GET /ends");
+        const cards_res = this.db_mongo.collection('End')
+        cards_res.find().toArray((err, items) => {
+          // console.log(items)
+          res.send(items)
+      })
+      // res.send(all_cards);
     });
 
     this.app.get('/characters', (req, res) => {
